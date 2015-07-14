f1a20a9c915966a02a6dbf07ddf0c1e620379000
HyeongJH <krack1@naver.com>
2015-07-14 14:18:36 +0900

    up

--- app/src/main/java/com/philips/lighting/quickstart/MyApplicationActivity.java
+++ app/src/main/java/com/philips/lighting/quickstart/MyApplicationActivity.java
@@ -1,15 +1,13 @@
 package com.philips.lighting.quickstart;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.philips.lighting.hue.listener.PHLightListener;
 import com.philips.lighting.hue.sdk.PHHueSDK;
@@ -18,6 +16,9 @@ import com.philips.lighting.model.PHBridgeResource;
 import com.philips.lighting.model.PHHueError;
 import com.philips.lighting.model.PHLight;
 import com.philips.lighting.model.PHLightState;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * MyApplicationActivity - The starting point for creating your own Hue App.  
@@ -30,7 +31,7 @@ public class MyApplicationActivity extends Activity {
     private PHHueSDK phHueSDK;
     private static final int MAX_HUE=65535;
     public static final String TAG = "QuickStart";
-    
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,27 +44,46 @@ public class MyApplicationActivity extends Activity {
 
             @Override
             public void onClick(View v) {
-                randomLights();
+                setLights();
             }
 
         });
 
     }
 
-    public void randomLights() {
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.w(TAG, "Inflating home menu");
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+
+    public void setLights() {
+        EditText textHue;
+        EditText textBri;
+        EditText textSat;
+        EditText textId;
+
+        textHue = (EditText) findViewById(R.id.edit_hue);
+        textBri = (EditText) findViewById(R.id.edit_bri);
+        textSat = (EditText) findViewById(R.id.edit_sat);
+        textId = (EditText) findViewById(R.id.edit_Id);
+
         PHBridge bridge = phHueSDK.getSelectedBridge();
 
-        List<PHLight> allLights = bridge.getResourceCache().getAllLights();
-        Random rand = new Random();
-        
-        for (PHLight light : allLights) {
+
+        String light = textId.getText().toString();
+
             PHLightState lightState = new PHLightState();
-            lightState.setHue(rand.nextInt(MAX_HUE));
+            lightState.setHue(Integer.parseInt(textHue.getText().toString()));
+            lightState.setBrightness(Integer.parseInt(textBri.getText().toString()));
+            lightState.setSaturation(Integer.parseInt(textSat.getText().toString()));
             // To validate your lightstate is valid (before sending to the bridge) you can use:  
             // String validState = lightState.validateState();
             bridge.updateLightState(light, lightState, listener);
             //  bridge.updateLightState(light, lightState);   // If no bridge response is required then use this simpler form.
-        }
+
     }
     // If you want to handle the response from the bridge, create a PHLightListener object.
     PHLightListener listener = new PHLightListener() {
